7919993ebd3f1365f9357134843e91cb95bd8ddf
Mike Pruyn <mpruyn@umass.edu>
2020-03-11 13:06:51 -0400

    adding files

---
+++ sent_anal.py
@@ -0,0 +1,103 @@
+import xml.etree.ElementTree as ET
+import re
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+from sklearn.feature_extraction.text import CountVectorizer
+import string
+from nltk.tokenize import word_tokenize
+import nltk
+from collections import defaultdict
+# nltk.download('punkt')
+
+def clean_up(text):
+    #convert to lower-case
+    text = text.lower()
+    #remove numbers
+    text = re.sub(r'\d+', '', text)
+    #remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    
+    return text
+
+def parse_review(filepath, in_vocab=None):
+    tree = ET.parse(filepath)
+    root = tree.getroot()
+
+    vocab = defaultdict(int)
+    tokenized_reviews = []
+    labels = []
+    clean_reviews = []
+
+    for review in root.findall('review'):
+        #grab review text and star rating for each review
+        text = review.find('review_text').text
+        stars = int(float(review.find('rating').text.strip('\n')))
+
+        #convert star rating to pos/neg label and add to list of labels
+        if stars == 3: continue
+        label = 1 if stars > 3 else -1
+        labels.append(label)
+
+        #clean up review text 
+        clean_text = clean_up(text)
+        clean_reviews.append(clean_text)
+
+        #tokenize review text
+        tokens = word_tokenize(clean_text)
+
+        #remove stop words
+        tokens = [token for token in tokens if token not in ENGLISH_STOP_WORDS]
+
+        #add every token to the vocab counts
+        for token in tokens:
+            vocab[token] += 1
+
+        #add the tokenized review to the list
+        tokenized_reviews += tokens
+
+    #removes words that appear less than 3 times from vocabulary 
+    for word_type in vocab:
+        if vocab[word_type] < 3: 
+            vocab[word_type] = 0
+    #subsequently removes such words from the reviews
+    tokenized_without_outliers = []
+    for review in tokenized_reviews:
+        review = [token for token in review if vocab[token] > 0]
+        tokenized_without_outliers.append(review)
+
+    reviews = tokenized_without_outliers
+    
+
+    if in_vocab:
+        cv = CountVectorizer(binary=True, vocabulary=in_vocab)
+    else:
+        cv = CountVectorizer(binary=True)
+    cv.fit(clean_reviews)
+    vocab = cv.vocabulary_
+    X = cv.transform(clean_reviews)
+    Y = labels
+    return X, Y, vocab
+
+X, Y, vocab = parse_review('sorted_data/apparel/all.review')
+X_baby, Y_baby, vocab = parse_review('sorted_data/baby/all.review', vocab)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+SVM = SVC(C=10000, gamma='auto')
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
+
+
+SVM.fit(X_train, y_train)
+y_pred = SVM.predict(X_test)
+score = accuracy_score(y_test, y_pred)
+print(score)
+
+y_pred_baby = SVM.predict(X_baby)
+baby_score = accuracy_score(Y_baby, y_pred_baby)
+print(baby_score)
+# print(cross_val_score(SVM, X, Y, cv=3))
